b1f97ecdfa9e59f431ec8e88937eca5324027e85
Paul Mastavy <gammabeam82@gmail.com>
2017-02-04 18:57:41 +0300

    Optimized templates.

--- web/assets/js/books-index.js
+++ web/assets/js/books-index.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
 	$('.chosen-select').chosen();
 
-	$('.delete').on('click', function(e) {
-		$('.delete-book').attr('href', $(this).attr('data-path'));
-		$('.book-name').html(
-			$(this).attr('data-name')
-		);
-	});
-
 	$('.datepicker').datetimepicker({
 		format: 'DD.MM.YYYY',
 		locale: 'ru'
