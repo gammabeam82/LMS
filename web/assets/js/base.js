5880b3349dbf78fead41c5b037b27edfce8eb5c2
Paul Mastavy <gammabeam82@gmail.com>
2017-04-07 16:29:47 +0300

    Installed bootstrap notify.

--- web/assets/js/base.js
+++ web/assets/js/base.js
@@ -3,9 +3,8 @@ $(document).ready( () => {
 
 	const booksCountContainer = $('.books-count');
 	const bookUrl = booksCountContainer.attr('data-path');
-	const archiveModal = $('#archive-modal');
 
-	
+
 	$('.to-archive').click(function() {
 		let btn = $(this);
 		if(!btn.hasClass('disabled')) {
@@ -13,9 +12,7 @@ $(document).ready( () => {
 				if(data['booksCount']) {
 					booksCountContainer.html(data['booksCount']);
 					btn.addClass('disabled');
-					archiveModal.find('.modal-body').html(data['message']);
-					archiveModal.modal('show');
-					setTimeout(() => archiveModal.modal('hide'), 1000);
+					$.notify(data['message'], { type: 'success' });
 				}
 			});
 		}
@@ -26,6 +23,7 @@ $(document).ready( () => {
 		$.get(btn.attr('data-path'), (data) => {
 			if(data['message']) {
 				btn.closest('tr').remove();
+				$.notify(data['message'], { type: 'success' });
 				booksCountContainer.html(data['booksCount']);
 				if(data['booksCount'] === 0) {
 					$('.archive-download').addClass('disabled');
