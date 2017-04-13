5b3faa292fdaa5779f56ae27b02fe8229133457a
Paul Mastavy <gammabeam82@gmail.com>
2017-04-13 14:50:44 +0300

    Removed rounded corners.

--- web/assets/js/base.js
+++ web/assets/js/base.js
@@ -1,9 +1,9 @@
-$(document).ready( () => {
+$(document).ready(() => {
 
 	const booksCountContainer = $('.books-count');
 	const errorMessage = $('.common-messages-error').text();
 
-	$(document).ajaxError( () => $.notify(errorMessage, { type: 'danger' }));
+	$(document).ajaxError(() => $.notify(errorMessage, {type: 'danger'}));
 
 	$('[data-toggle="tooltip"]').tooltip({
 		delay: {
@@ -12,43 +12,46 @@ $(document).ready( () => {
 		}
 	});
 
-	$('.to-archive').click(function() {
+	$('.to-archive').click(function () {
 		let btn = $(this);
-		if(!btn.hasClass('disabled')) {
-			$.getJSON(btn.attr('data-path'), (data) => {
-				if(typeof data['booksCount'] !== 'undefined') {
-					booksCountContainer.html(data['booksCount']);
-					btn.addClass('disabled');
-					$.notify(data['message'], { type: 'success' });
-					if(!booksCountContainer.hasClass('badge-active')) {
-						booksCountContainer.addClass('badge-active');
+		if (!btn.hasClass('disabled')) {
+			$.getJSON(btn.attr('data-path'))
+				.then((data) => {
+					if (typeof data['booksCount'] !== 'undefined') {
+						booksCountContainer.html(data['booksCount']);
+						btn.addClass('disabled');
+						$.notify(data['message'], {type: 'success'});
+						if (!booksCountContainer.hasClass('badge-active')) {
+							booksCountContainer.addClass('badge-active');
+						}
+						booksCountContainer.addClass('animated bounceIn');
+						setTimeout(() => booksCountContainer.removeClass('animated bounceIn'), 600);
+					} else {
+						$.notify(errorMessage, {type: 'danger'});
 					}
-					booksCountContainer.addClass('animated bounceIn');
-					setTimeout(() => booksCountContainer.removeClass('animated bounceIn'), 600);
-				} else {
-					$.notify(errorMessage, { type: 'danger' });
-				}
-			});
+				});
 		}
 	});
 
-	$('.remove-from-archive').click(function() {
+	$('.remove-from-archive').click(function () {
 		let btn = $(this);
-		$.getJSON(btn.attr('data-path'), (data) => {
-			if(typeof data['booksCount'] !== 'undefined') {
-				let item = btn.closest('tr');
-				item.addClass('animated fadeOutLeft');
-				$.notify(data['message'], { type: 'info' });
-				setTimeout(() => item.remove(), 800);
-				booksCountContainer.html(data['booksCount']);
-				if(data['booksCount'] === 0) {
-					$('.archive-download').addClass('disabled');
-					booksCountContainer.removeClass('badge-active');
+		$.getJSON(btn.attr('data-path'))
+			.then((data) => {
+				if (typeof data['booksCount'] !== 'undefined') {
+					let item = btn.closest('tr');
+					item.addClass('animated fadeOut');
+					$.notify(data['message'], {type: 'info'});
+					setTimeout(() => item.remove(), 800);
+					booksCountContainer.html(data['booksCount']);
+					if (data['booksCount'] === 0) {
+						$('.archive-download').addClass('disabled');
+						booksCountContainer.removeClass('badge-active');
+						setTimeout(() => $('table').remove(), 800);
+					}
+				} else {
+					$.notify(errorMessage, {type: 'danger'});
 				}
-			} else {
-				$.notify(errorMessage, { type: 'danger' });
-			}
-		});
+			});
 	});
 
 });
