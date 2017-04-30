621aedb21f28d12217f7e5714dfaf6af84558064
Paul Mastavy <gammabeam82@gmail.com>
2017-04-30 13:40:29 +0300

    Created PDO exception listener.

---
+++ src/AppBundle/EventListener/PDOExceptionListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Doctrine\DBAL\Exception\ConnectionException;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class PDOExceptionListener
+{
+	/**
+	 * @param GetResponseForExceptionEvent $event
+	 */
+	public function onPDOException(GetResponseForExceptionEvent $event)
+	{
+		$exception = $event->getException();
+
+		if ($exception instanceof ConnectionException) {
+			//TODO create error page
+		}
+	}
+}
