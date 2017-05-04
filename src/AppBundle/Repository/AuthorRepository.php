b4fe171891ee683fa3e6b171bbfd3f46a91503ad
Paul Mastavy <gammabeam82@gmail.com>
2017-05-04 20:44:14 +0300

    Updated authors repository.

--- src/AppBundle/Repository/AuthorRepository.php
+++ src/AppBundle/Repository/AuthorRepository.php
@@ -4,5 +4,19 @@ namespace AppBundle\Repository;
 
 class AuthorRepository extends AbstractRepository
 {
+	/**
+	 * @param $letter
+	 * @return array
+	 */
+	public function findAllStartsWith($letter)
+	{
+		$qb = $this->createQueryBuilder('a');
 
+		$qb->andWhere($qb->expr()->like('LOWER(a.lastName)', ':name'));
+		$qb->setParameter('name', mb_strtolower($letter) . "%");
+
+		return $qb
+			->getQuery()
+			->getResult();
+	}
 }
