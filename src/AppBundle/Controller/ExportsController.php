6bad402893d0b0bc33e8ff6c2a6547a0d01d69f2
Paul Mastavy <gammabeam82@gmail.com>
2017-05-17 17:39:18 +0300

    Updated export.

---
+++ src/AppBundle/Controller/ExportsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+class ExportsController extends Controller
+{
+
+	/**
+	 * @Route("/export", name="export")
+	 *
+	 * @return Response
+	 */
+	public function indexAction()
+	{
+		return $this->render('exports/index.html.twig', []);
+	}
+
+	/**
+	 * @Route("/export/authors", name="export_authors")
+	 *
+	 * @return BinaryFileResponse
+	 */
+	public function authorsExportAction()
+	{
+		$authorsService = $this->get('app.authors');
+
+		$file = $authorsService->export();
+
+		$response = new BinaryFileResponse($file);
+		$response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
+
+		return $response;
+	}
+
+}
