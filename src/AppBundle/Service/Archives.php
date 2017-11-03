7cd34cb8aa70c99233d73fb425842d178b5e22e3
Paul Mastavy <gammabeam82@gmail.com>
2017-11-03 21:00:37 +0300

    Formatting.

--- src/AppBundle/Service/Archives.php
+++ src/AppBundle/Service/Archives.php
@@ -13,10 +13,10 @@ use ZipStream\ZipStream;
 class Archives
 {
 
-	/**
-	 * @var Registry
-	 */
-	private $doctrine;
+    /**
+     * @var Registry
+     */
+    private $doctrine;
 
     /**
      * @var \Predis\Client
@@ -34,101 +34,101 @@ class Archives
      * @param TokenStorageInterface $tokenStorage
      * @param \Predis\Client $redis
      */
-	public function __construct(Registry $doctrine, TokenStorageInterface $tokenStorage, \Predis\Client $redis)
-	{
-		$this->doctrine = $doctrine;
-		$this->redis = $redis;
-		$this->key = sprintf("archive:%s", $tokenStorage->getToken()->getUser()->getId());
-	}
+    public function __construct(Registry $doctrine, TokenStorageInterface $tokenStorage, \Predis\Client $redis)
+    {
+        $this->doctrine = $doctrine;
+        $this->redis = $redis;
+        $this->key = sprintf("archive:%s", $tokenStorage->getToken()->getUser()->getId());
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getBooksCount()
-	{
-		return $this->redis->hlen($this->key);
-	}
+    /**
+     * @return int
+     */
+    public function getBooksCount()
+    {
+        return $this->redis->hlen($this->key);
+    }
 
-	/**
-	 * @param Book $book
-	 */
-	public function addBookToArchive(Book $book)
-	{
-		$this->redis->hsetnx($this->key, $book->getId(), $book->getName());
-	}
+    /**
+     * @param Book $book
+     */
+    public function addBookToArchive(Book $book)
+    {
+        $this->redis->hsetnx($this->key, $book->getId(), $book->getName());
+    }
 
-	/**
-	 * @param Book $book
-	 */
-	public function removeBookFromArchive(Book $book)
-	{
+    /**
+     * @param Book $book
+     */
+    public function removeBookFromArchive(Book $book)
+    {
         $this->redis->hdel($this->key, [$book->getId()]);
-	}
+    }
 
-	/**
-	 * @return array|bool
-	 */
-	public function getBooksList()
-	{
-		if (0 === $this->redis->hlen($this->key)) {
-			return false;
-		}
+    /**
+     * @return array|bool
+     */
+    public function getBooksList()
+    {
+        if (0 === $this->redis->hlen($this->key)) {
+            return false;
+        }
 
         return $this->redis->hgetall($this->key);
-	}
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getBookIds()
-	{
-		return $this->redis->hkeys($this->key);
-	}
+    /**
+     * @return array
+     */
+    public function getBookIds()
+    {
+        return $this->redis->hkeys($this->key);
+    }
 
-	/**
-	 * @return BinaryFileResponse
-	 */
-	public function getArchive()
-	{
-		if (0 === $this->redis->hlen($this->key) || false === $this->redis->exists($this->key)) {
-			throw new LengthException();
-		}
+    /**
+     * @return BinaryFileResponse
+     */
+    public function getArchive()
+    {
+        if (0 === $this->redis->hlen($this->key) || false === $this->redis->exists($this->key)) {
+            throw new LengthException();
+        }
 
-		/* @var \Doctrine\ORM\EntityRepository $repo */
-		$repo = $this->doctrine->getRepository(Book::class);
+        /* @var \Doctrine\ORM\EntityRepository $repo */
+        $repo = $this->doctrine->getRepository(Book::class);
 
-		$em = $this->doctrine->getManager();
+        $em = $this->doctrine->getManager();
 
-		$qb = $repo->createQueryBuilder('b');
-		$qb->where($qb->expr()->in('b.id', $this->getBookIds()));
+        $qb = $repo->createQueryBuilder('b');
+        $qb->where($qb->expr()->in('b.id', $this->getBookIds()));
 
-		$books = $qb->getQuery()->execute();
+        $books = $qb->getQuery()->execute();
 
-		array_map(function ($book) use ($em) {
-			/* @var \AppBundle\Entity\Book $book */
-			$book->incViews();
-			$em->persist($book);
-		}, $books);
+        array_map(function ($book) use ($em) {
+            /* @var \AppBundle\Entity\Book $book */
+            $book->incViews();
+            $em->persist($book);
+        }, $books);
 
-		$em->flush();
+        $em->flush();
 
-		$zipStream = new ZipStream("books.zip");
+        $zipStream = new ZipStream("books.zip");
 
-		foreach ($books as $book) {
-			/* @var \AppBundle\Entity\Book $book */
-			foreach ($book->getBookFiles() as $bookFile) {
-				/* @var \AppBundle\Entity\File $bookFile */
-				if (false === $bookFile->getIsImage()) {
-					$localname = sprintf("%s-%s.%s", $book->getAuthor()->getShortName(), $book->getName(), $bookFile->getType());
-					$zipStream->addFileFromPath($localname, $bookFile->getName());
-				}
-			}
-		}
+        foreach ($books as $book) {
+            /* @var \AppBundle\Entity\Book $book */
+            foreach ($book->getBookFiles() as $bookFile) {
+                /* @var \AppBundle\Entity\File $bookFile */
+                if (false === $bookFile->getIsImage()) {
+                    $localname = sprintf("%s-%s.%s", $book->getAuthor()->getShortName(), $book->getName(), $bookFile->getType());
+                    $zipStream->addFileFromPath($localname, $bookFile->getName());
+                }
+            }
+        }
 
-		$response = new BinaryFileResponse($zipStream->finish());
-		$response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
+        $response = new BinaryFileResponse($zipStream->finish());
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
 
-		return $response;
-	}
+        return $response;
+    }
 
 }
