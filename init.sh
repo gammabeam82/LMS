862a77af75b7cefc05e85f34e70849b2eca77de4
Paul Mastavy <gammabeam82@gmail.com>
2018-01-07 16:42:56 +0300

    App initialization script.

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+rm -rf var/cache/*
+rm -rf var/logs/*
+HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+
+mkdir -p var/jwt
+openssl genrsa -out var/jwt/private.pem -aes256 4096
+openssl rsa -pubout -in var/jwt/private.pem -out var/jwt/public.pem
+
+php bin/console doctrine:database:drop --force --if-exists
+php bin/console doctrine:database:create --no-interaction --if-not-exists
+php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console doctrine:fixtures:load --no-interaction
+php bin/console fos:user:create testuser test@test.test p@ssword
+php bin/console fos:user:promote testuser ROLE_ADMIN
+php bin/console cache:clear --env=dev --no-debug --no-warmup
+php bin/console cache:clear --env=prod --no-debug --no-warmup
