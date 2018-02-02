e3f98fc5017706b593bbe11b444243c4ddf11c4e
Paul Mastavy <gammabeam82@gmail.com>
2018-02-02 17:54:42 +0300

    Updated transformer.

--- src/AppBundle/Api/Transformer/BookTransformer.php
+++ src/AppBundle/Api/Transformer/BookTransformer.php
@@ -5,6 +5,8 @@ namespace AppBundle\Api\Transformer;
 use AppBundle\Entity\Book;
 use AppBundle\Entity\Genre;
 use AppBundle\Entity\Serie;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
 use League\Fractal\TransformerAbstract;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
@@ -18,11 +20,20 @@ class BookTransformer extends TransformerAbstract implements TransformerInterfac
 
     /**
      * BookTransformer constructor.
+     *
      * @param RouterInterface $router
      */
     public function __construct(RouterInterface $router)
     {
         $this->router = $router;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDefaultIncludes(): array
+    {
+        return ['author', 'genre', 'serie', 'textFiles'];
     }
 
     /**
@@ -32,35 +43,57 @@ class BookTransformer extends TransformerAbstract implements TransformerInterfac
      */
     public function transform(Book $book): array
     {
-        $genre = $book->getGenre();
-        $serie = $book->getSerie();
-
-        $authorTransformer = new AuthorTransformer();
-        $fileTransformer = new FileTransformer($this->router);
-
         $data = [
             'id' => $book->getId(),
             'name' => $book->getName(),
             'createdAt' => $book->getCreatedAt()->getTimestamp(),
             'url' => $this->router->generate('books_view', ['id' => $book->getId()], UrlGeneratorInterface::ABSOLUTE_URL),
-            'author' => $authorTransformer->transform($book->getAuthor()),
-            'files' => array_map(function ($file) use ($fileTransformer) {
-                return $fileTransformer->transform($file);
-            }, array_values($book->getTextFiles())),
-            'genre' => null,
-            'serie' => null
         ];
 
-        if (false !== $genre instanceof Genre) {
-            $genreTransformer = new GenreTransformer();
-            $data['genre'] = $genreTransformer->transform($genre);
-        }
-
-        if (false !== $serie instanceof Serie) {
-            $serieTransformer = new SerieTransformer();
-            $data['serie'] = $serieTransformer->transform($serie);
-        }
-
         return $data;
+    }
+
+    /**
+     * @param Book $book
+     *
+     * @return Item
+     */
+    public function includeAuthor(Book $book): Item
+    {
+        return new Item($book->getAuthor(), new AuthorTransformer());
+    }
+
+    /**
+     * @param Book $book
+     *
+     * @return Collection
+     */
+    public function includeTextFiles(Book $book): Collection
+    {
+        return new Collection($book->getTextFiles(), new FileTransformer($this->router));
+    }
+
+    /**
+     * @param Book $book
+     *
+     * @return Item
+     */
+    public function includeGenre(Book $book): Item
+    {
+        $genre = $book->getGenre() ?? new Genre();
+
+        return new Item($genre, new GenreTransformer());
+    }
+
+    /**
+     * @param Book $book
+     *
+     * @return Item
+     */
+    public function includeSerie(Book $book): Item
+    {
+        $serie = $book->getSerie() ?? new Serie();
+
+        return new Item($serie, new SerieTransformer());
     }
 }
